7783aed2ff24e125e79740167f522e5c950c0d75
Jarmo Isotalo <jamo@isotalo.fi>
2013-11-02 10:03:47 +0200

    init

---
+++ doodle-lite/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DoodleLite::Application.config.secret_key_base = '215e91756a9ee0b26db0dcc3c3f740698ba7c28841940f321d3530f23162856ca013ec8cf9d01ca18243d15206bd303628362ecc9a67883918ecf092ce90142c'
---
+++ doodle-lite/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DoodleLite::Application.config.session_store :cookie_store, key: '_doodle-lite_session'
